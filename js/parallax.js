e705e2d438cc9b227658a76beb40c27d8686acde
ChrisComeTacos <leanyuwu@gmail.com>
2025-03-13 14:14:38 -0600

    Se modifica el header del index y su responsive

--- js/parallax.js
+++ js/parallax.js
@@ -1,7 +1,8 @@
 // let header = document.getElementById("header");
 
-let tituloHeaderPagos = document.getElementById("tituloHeaderPagos");
-let subtituloHeaderPagos = document.getElementById("subtituloHeaderPagos");
+let indexHeader_titulo = document.getElementById("indexHeader_titulo");
+let indexHeader_subtitulo = document.getElementById("indexHeader_subtitulo");
+let indexHeader_btns = document.getElementById("indexHeader_btns");
 let cerros = document.getElementById("cerros");
 let persona1 = document.getElementById("persona1");
 let persona2 = document.getElementById("persona2");
@@ -10,8 +11,9 @@ window.addEventListener("scroll", () => {
   let value = window.scrollY;
 
   cerros.style.marginTop = value * 0.5 + "px";
-  tituloHeaderPagos.style.marginTop = value * 0.7 + "px";
-  subtituloHeaderPagos.style.marginTop = value * 1 + "px";
+  indexHeader_titulo.style.marginTop = value * 0.7 + "px";
+  indexHeader_subtitulo.style.marginTop = value * 0.7 + "px";
+  indexHeader_btns.style.marginTop = value * 0.2 + "px";
   persona1.style.left = value * 1 + "px";
   persona2.style.left = value * -1 + "px";
   universidad.style.marginTop = value * 0.5 + "px";
